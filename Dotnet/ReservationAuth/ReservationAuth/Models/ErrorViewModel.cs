cf677e92fd7f495e124ccaba4035f9e3b773d148
NIRMALRAJ1506 <nirmalraj75500@gmail.com>
2024-08-30 17:31:17 +0530

    committed

---
+++ Dotnet/ReservationAuth/ReservationAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReservationAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Dotnet/ReservationDemoApi/ReservationDemoApi/Models/ReservationContext.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+using Microsoft.EntityFrameworkCore;
+
+namespace ReservationDemoApi.Models
+{
+    public class ReservationContext : DbContext
+    {
+        public ReservationContext(DbContextOptions<ReservationContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Reservation> Reservation { get; set; }
+
+    }
+}
+
